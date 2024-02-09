6aa85b6cda07850380d05a355b31e2f1c73ba32c
Gustavo Luz <henriqueglfs@gmail.com>
2024-02-09 18:10:30 -0400

    Handling exceptions and clearing screen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -43,7 +43,7 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 
 		if (!board.thereIsAPiece(position)) {
-			throw new ChessException("There is no piecec on source position");
+			throw new ChessException("There is no piece on source position");
 		}
 	}
 
@@ -54,19 +54,19 @@ public class ChessMatch {
 	private void initialSetup() {
 //		TESTING: 
 
-//		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
-//		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-//		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-//		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-//		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
-//		placeNewPiece('d', 1, new King(board, Color.WHITE));
-//		
-//		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
-//		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-//		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-//		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-//		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-//		placeNewPiece('d', 8, new King(board, Color.BLACK));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+		
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
 
 	}
 
